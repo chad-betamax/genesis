3b361bf2d2c605c88973226e7a7bedbf72034e21
Doug <doug@phoenox.net>
2022-05-01 22:16:27 +0800

    more tutorial

--- dodo.py
+++ dodo.py
@@ -1,14 +1,14 @@
 def task_hello():
     def task_hello():
-    """hello"""
+        """hello"""
 
-    def python_hello(targets):
-        with open(targets[0], "a") as output:
-            output.write("Python says Hello World!!!\n")
+        def python_hello(targets):
+            with open(targets[0], "a") as output:
+                output.write("Python says Hello World!!!\n")
 
-    return {
-        'actions': [python_hello],
-        'targets': ["hello.txt"],
-        }
+        return {
+            'actions': [python_hello],
+            'targets': ["hello.txt"],
+            }
 
 
